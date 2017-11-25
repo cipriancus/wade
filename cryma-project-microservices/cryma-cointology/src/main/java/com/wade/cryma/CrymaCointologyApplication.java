40c05ceb0de41d61d7ef0866701aaa0aeb2ac218
cocageorgiana <cocageorgiana@gmail.com>
2017-11-25 16:03:06 +0200

    create zuul gateway api and eureka, configure microservice to eureka

--- cryma-project-microservices/cryma-cointology/src/main/java/com/wade/cryma/CrymaCointologyApplication.java
+++ cryma-project-microservices/cryma-cointology/src/main/java/com/wade/cryma/CrymaCointologyApplication.java
@@ -2,8 +2,11 @@ package com.wade.cryma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CrymaCointologyApplication {
 
 	public static void main(String[] args) {
--- cryma-project-microservices/cryma-eureka-service/src/main/java/com/wade/cryma/EurekaServiceApplication.java
+++ cryma-project-microservices/cryma-eureka-service/src/main/java/com/wade/cryma/EurekaServiceApplication.java
@@ -2,10 +2,13 @@ package com.wade.cryma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+
 @SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
