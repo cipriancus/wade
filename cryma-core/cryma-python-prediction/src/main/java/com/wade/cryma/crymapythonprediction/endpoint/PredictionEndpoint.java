e4e166905108361542cd0e744cdad5c51d586fd3
cocageorgiana <cocageorgiana@gmail.com>
2018-01-06 21:36:53 +0200

    Request to python library

--- cryma-core/cryma-python-prediction/src/main/java/com/wade/cryma/crymapythonprediction/endpoint/PredictionEndpoint.java
+++ cryma-core/cryma-python-prediction/src/main/java/com/wade/cryma/crymapythonprediction/endpoint/PredictionEndpoint.java
@@ -1,4 +1,36 @@
 package com.wade.cryma.crymapythonprediction.endpoint;
 
+import com.wade.cryma.crymapythonprediction.library.LibraryRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
 public class PredictionEndpoint {
+
+    private LibraryRequest libraryRequest;
+
+    @Autowired
+    public PredictionEndpoint(LibraryRequest libraryRequest) {
+        this.libraryRequest = libraryRequest;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/predict/{coinId}")
+    public @ResponseBody
+    List<Integer> predict(@RequestParam(value = "coinId", required = true) String coinId) {
+        List<Integer> predictionResult=libraryRequest.sendLibraryRequest(coinId);
+
+        if(predictionResult!=null&&predictionResult.size()>0){
+            return predictionResult;
+        }
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT);
+    }
+
 }
--- cryma-core/cryma-python-prediction/src/main/java/com/wade/cryma/crymapythonprediction/library/LibraryRequest.java
+++ cryma-core/cryma-python-prediction/src/main/java/com/wade/cryma/crymapythonprediction/library/LibraryRequest.java
@@ -1,4 +1,26 @@
 package com.wade.cryma.crymapythonprediction.library;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
 public class LibraryRequest {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private String url;
+
+    @Autowired
+    public LibraryRequest(@Value("#{systemProperties['library.url']}") String url){
+        this.url=url;
+    }
+
+    public List<Integer> sendLibraryRequest(String coinId){
+        List<Integer> predictionList=restTemplate.getForObject(url,List.class);
+        return predictionList;
+    }
 }
