bc80d46b10ae423d7b4804925304db29fc9d65df
cipriancus <cipriancus@gmail.com>
2018-01-07 13:07:25 +0200

    Cryma-python-prediction
    Microservice for making request at the python library
    Made eureka work and register

--- cryma-core/cryma-python-prediction/src/main/java/com/wade/cryma/crymapythonprediction/endpoint/PredictionEndpoint.java
+++ cryma-core/cryma-python-prediction/src/main/java/com/wade/cryma/crymapythonprediction/endpoint/PredictionEndpoint.java
@@ -1,36 +1,30 @@
 package com.wade.cryma.crymapythonprediction.endpoint;
 
+import com.wade.cryma.crymapythonprediction.exception.RequestNoResponseException;
 import com.wade.cryma.crymapythonprediction.library.LibraryRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-@Controller
+@RestController
 public class PredictionEndpoint {
 
+    @Autowired
     private LibraryRequest libraryRequest;
 
-    @Autowired
-    public PredictionEndpoint(LibraryRequest libraryRequest) {
-        this.libraryRequest = libraryRequest;
-    }
+    @RequestMapping(method = RequestMethod.GET, path = "predict/{coinId}",produces = "application/json")
+    public Map<String,List<String>> predict(@PathVariable(value = "coinId", required = true) String coinId) {
+        Map<String,List<String>> predictionResult = libraryRequest.sendLibraryRequest(coinId);
 
-    @RequestMapping(method = RequestMethod.GET, path = "/predict/{coinId}")
-    public @ResponseBody
-    List<Integer> predict(@RequestParam(value = "coinId", required = true) String coinId) {
-        List<Integer> predictionResult=libraryRequest.sendLibraryRequest(coinId);
-
-        if(predictionResult!=null&&predictionResult.size()>0){
+        if (predictionResult != null && predictionResult.size() > 0) {
             return predictionResult;
         }
-        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT);
+        throw new RequestNoResponseException();
     }
-
 }
--- cryma-core/cryma-python-prediction/src/main/java/com/wade/cryma/crymapythonprediction/library/LibraryRequest.java
+++ cryma-core/cryma-python-prediction/src/main/java/com/wade/cryma/crymapythonprediction/library/LibraryRequest.java
@@ -5,22 +5,27 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class LibraryRequest {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${library.url}")
     private String url;
 
-    @Autowired
-    public LibraryRequest(@Value("#{systemProperties['library.url']}") String url){
-        this.url=url;
-    }
-
-    public List<Integer> sendLibraryRequest(String coinId){
-        List<Integer> predictionList=restTemplate.getForObject(url,List.class);
-        return predictionList;
+    public Map<String,List<String>> sendLibraryRequest(String coinId) {
+        try {
+            //url = url + coinId;
+            Map<String,List<String>> predictionList = restTemplate.getForObject(url, Map.class);
+            return predictionList;
+        }catch (Exception e){
+            e.printStackTrace();
+            return new HashMap<>();
+        }
     }
 }
