abe0c65428844b82ce00a3d6ddda22a1c790d172
cipriancus <cipriancus@gmail.com>
2018-01-15 19:53:18 +0200

    Cleanup & Cross Origin Enable

--- cryma-core/cryma-plot/src/main/java/com/wade/cryma/crymaplot/repository/TopCoinRepository.java
+++ cryma-core/cryma-plot/src/main/java/com/wade/cryma/crymaplot/repository/TopCoinRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "coin", path = "coin")
 public interface TopCoinRepository extends CrudRepository<TopCoin,Long> {
     Page<TopCoin> findAll(Pageable page);
